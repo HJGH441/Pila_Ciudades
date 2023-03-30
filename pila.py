f580b529c9b7d34a645678ea201e0c963ff7f07a
Héctor Jesús González Haro <hectorjesusharo441@gmail.com>
2023-03-30 17:26:53 -0600

    2

--- pila.py
+++ pila.py
@@ -20,6 +20,53 @@ Ciudades_Caminos = {
     'Reynosa': ['Matamoros']
 }
 
+Ciudades_Caminos2 = {
+    'Salina Cruz': ['Tehuanepec','Huatulco'],
+    'Huatulco': ['Salina Cruz','Puerto Escondido'],
+    'Puerto Escondido': ['Huatulco'],
+    'Tehuanepec': ['Salina Cruz', 'Oaxaca','Juchitan'],
+    'Juchitan': ['Ixtepec','Tonala', 'Tehuanepec','Acayucan'],
+    'Acayucan': ['Juchitan','Veracruz','Minatitlan'],
+    'Oaxaca': ['Tehuanepec','Puebla'],
+    'Puebla': ['Cd. Mexico','Orizaba', 'Oaxaca'],
+    'Cd. Mexico': ['Puebla'],
+    'Orizaba': ['Puebla','Cordoba'],
+    'Cordoba': ['Orizaba','Veracruz'],
+    'Veracruz': ['Cordoba','Jalapa', 'Poza Rica','Acayucan'],
+    'Jalapa': ['Veracruz'],
+    'Poza Rica': ['Veracruz', 'Tuxpan'],
+    'Tuxpan': ['Poza Rica', 'Tampico'],
+    'Tampico': ['Matamoros', 'Tuxpan'],
+    'Matamoros': ['Tampico', 'Reynosa'],
+    'Reynosa': ['Matamoros'],
+    'Ixtepec': ['Ixcatepec', 'Juchitan'],
+    'Ixcatepec': ['Ixtepec'],
+    'Tonala': ['Tuxla Gutierrez','Pijijipan', 'Juchitan'],
+    'Pijijipan': ['Huixtla', 'Tonala'],
+    'Huixtla': ['Pijijipan', 'Tapachula'],
+    'Tapachula': ['Huixtla'],
+    'Tuxla Gutierrez': ['Villahermosa','Tonala'],
+    'Villahermosa': ['Cardenaz','Palenque', 'Cd. del Carmen','Tuxla Gutierrez'],
+    'Cardenaz': ['Coatzacoalcos','Villahermosa'],
+    'Coatzacoalcos': ['Minatitlan','Cardenaz'],
+    'Minatitlan': ['Acayucan','Coatzacoalcos'],
+    'Palenque': ['Ococingo','Villahermosa'],
+    'Ococingo': ['Palenque','San Cristobal'],
+    'San Cristobal': ['Comitan','Ococingo'],
+    'Comitan': ['San Cristobal'],
+    'Cd. del Carmen': ['Villahermosa','Campeche'],
+    'Campeche': ['Chetumal','Merida', 'Cd. del Carmen'],
+    'Chetumal': ['Campeche','Playa del carmen'],
+    'Playa del carmen': ['Chetumal','Cancun'],
+    'Cancun': ['Playa del carmen','Merida'],
+    'Merida': ['Campeche','Cancun']
+
+
+    
+
+
+}
+
 def busqueda(x, y, z):
     cola = deque()
     cola.append((y, []))
@@ -36,6 +83,7 @@ def busqueda(x, y, z):
             cola.append((vecino, camino + [actual]))
     return None
 
-camino = busqueda(Ciudades_Caminos,'Salina Cruz', 'Reynosa')
-
+camino = busqueda(Ciudades_Caminos2,'Salina Cruz', 'Reynosa')
 print(camino)
+
+
